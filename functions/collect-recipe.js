14be976c788dee1b3045eae9e9329a16344974b5
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2025-11-23 01:28:17 +1000

    Implement recipe collection from URL
    Fetch recipe data from provided URL and parse JSON-LD.

--- functions/collect-recipe.js
+++ functions/collect-recipe.js
@@ -3,8 +3,34 @@ export default (req, res) => {
         if (!req.url.includes('?')) throw 400;
         let recipeUrl = new URL(req.url.slice(req.url.indexOf('?') + 1));
         if (recipeUrl.protocol !== 'https:' && recipeUrl.protocol !== 'http:') throw 400;
-        res.status(200).send(`Collecting url: ${recipeUrl}`);
+        fetch(recipeUrl).then(r => r.text()).then(t => {
+            let p = new DOMParser();
+            let nodeList = p.parseFromString(t, 'text/html').querySelectorAll('script[type="application/ld+json"]');
+            for (let node of nodeList.values()) {
+                try {
+                    let obj = JSON.parse(node.textContent);
+                    let recipe = findRecipe(obj);
+                    if (recipe) {
+                        res.status(200).send(recipe);
+                        return;
+                    }
+                } catch {}
+            }
+            res.sendStatus(404);
+        );
     } catch {
         res.sendStatus(400);
     }
+}
+
+function findRecipe(obj) {
+    if (obj?.['@type'] === 'Recipe') {
+        return obj;
+	} else if (obj?.['@graph']) {
+        for (let o of obj['@graph']) {
+            let recipe = findRecipe(o);
+            if (recipe) return recipe;
+        }
+    }
+    return null;
 }
