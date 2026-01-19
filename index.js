2dfbc1e6c6b8adbe29a6583dfe3af0fff9465707
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2026-01-19 19:46:17 +1000

    Add support for cloudflare workers

---
+++ index.js
@@ -0,0 +1,29 @@
+import handleReq from "functions/collect-recipe.js";
+
+export default {
+  async fetch(request) {
+    let response = new ResponseShim();
+    await handleReq(request, response);
+
+    return response.data;
+  },
+};
+
+class ResponseShim {
+    options = {status: 200};
+    data = null;
+
+    status(num){
+        this.options.status = num;
+        return this;
+    }
+
+    send(message) {
+        if (typeof message === 'string') {
+            this.data = new Response(message, this.options)
+        } else {
+            this.data = Response.json(message, this.options);
+        }
+        
+    }
+}
