f15def7c54fd46ec78f8934cc462a9b7e04d243f
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2026-01-25 21:15:25 +1000

    Add setHeader() function to ResponseShim

--- cloudflare/index.js
+++ cloudflare/index.js
@@ -1,18 +1,22 @@
 import handleReq from "../collect-recipe.js";
 
 export default {
-  async fetch(request) {
-    let response = new ResponseShim();
-    await handleReq(request, response);
-    return response.data;
-  },
+    async fetch(request) {
+        let response = new ResponseShim();
+        await handleReq(request, response);
+        return response.data;
+    },
 };
 
 class ResponseShim {
-    options = {status: 200};
+    options = {status: 200, headers: {}};
     data = null;
 
-    status(num){
+    setHeader(name, value) {
+        this.options.headers[name] = value;
+    }
+
+    status(num) {
         this.options.status = num;
         return this;
     }
@@ -23,6 +27,5 @@ class ResponseShim {
         } else {
             this.data = Response.json(message, this.options);
         }
-        
     }
 }
